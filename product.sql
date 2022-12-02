93e0361904f82ae79c7915ac6d99f551ab15a3ec
Naoufel Boukri <boukri_n@etna-alternance.net>
2022-12-02 17:25:22 +0100

    Add address field in users Table

--- product.sql
+++ product.sql
@@ -16,6 +16,7 @@ CREATE TABLE users (
     username VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
+    address VARCHAR(255) NOT NULL,
     role_id INT,
     CONSTRAINT users_pk PRIMARY KEY (id)
 );
