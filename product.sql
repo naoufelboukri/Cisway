96e55dbf6e853f3865a67885d50bc2c6f7375cbf
Naoufel Boukri <boukri_n@etna-alternance.net>
2022-12-02 17:22:09 +0100

    Add SQL file using to create our Database

---
+++ product.sql
@@ -0,0 +1,45 @@
+
+CREATE DATABASE products;
+use products;
+
+-- tables
+-- Table: roles
+CREATE TABLE roles(
+    id INT NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    CONSTRAINT roles_pk PRIMARY KEY (id)
+);
+
+-- Table: users
+CREATE TABLE users (
+    id INT NOT NULL,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    role_id INT,
+    CONSTRAINT users_pk PRIMARY KEY (id)
+);
+
+-- Table: products
+CREATE TABLE products (
+    id INT NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    price DECIMAL(5,2) NOT NULL,
+    user_id INT,
+    CONSTRAINT product_pk PRIMARY KEY (id)    
+);
+
+-- foreign keys
+-- Reference: users_products (table: products)
+ALTER TABLE products ADD CONSTRAINT users_products
+    FOREIGN KEY (user_id)
+    REFERENCES users (id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE;
+
+-- Reference: users_roles (table: users)
+ALTER TABLE users ADD CONSTRAINT users_roles
+    FOREIGN KEY (role_id)
+    REFERENCES roles (id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE;
