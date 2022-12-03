4d11163d5b8e7818cc5a51cf00d70213406d2b98
Naoufel Boukri <boukri_n@etna-alternance.net>
2022-12-03 14:48:59 +0100

    Initialisation of database

---
+++ API/db.config.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql')
+
+const connection = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'root',
+    database: 'products',
+});
+
+connection.connect((err) => {
+    if (!err) {
+      console.log("Database is connected ... \n\n");  
+    } else {
+      console.log("Error connectinf database ... \n\n");  
+    }
+})
+
+module.exports = connection
--- API/index.js
+++ API/index.js
@@ -1,11 +1,20 @@
+// --- IMPORT ---
 const express = require('express');
-const app = express();
-const port = 3000;
+const DB = require('./db.config');
+// --------------
 
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+// --- ENDPOINTS ---
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  DB.query("SELECT username FROM users", (err, result, fields) => {
+    if (err) throw err;
+    res.send(result);
+  })
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.listen(3000, () => {
+  console.log(`Example app listening on port 3000`);
 })
