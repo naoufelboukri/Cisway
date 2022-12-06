28449257697fece68f43af9df9b9d4f086cd5c51
Naoufel Boukri <boukri_n@etna-alternance.net>
2022-12-06 21:04:01 +0100

    New Initialized project

---
+++ routes/users.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const users = require('../services/users');
+
+/* GET programming languages. */
+router.get('/', async function(req, res, next) {
+  try {
+    res.json(await users.getMultiple(req.query.page));
+  } catch (err) {
+    console.error(`Error while getting programming languages `, err.message);
+    next(err);
+  }
+});
+
+router.post('/register', async function(req, res, next) {
+  try {
+    res.json(await users.create(req.body));
+  } catch (err) {
+    console.error(`Error while creating user`, err.message);
+    next(err);
+  }
+});
+
+module.exports = router;
---
+++ services/users.js
@@ -0,0 +1,39 @@
+const db = require('./db');
+const helper = require('../helper');
+const config = require('../config');
+
+async function getMultiple(page = 1){
+  const offset = helper.getOffset(page, config.listPerPage);
+  const rows = await db.query(
+    `SELECT * FROM users LIMIT ${offset},${config.listPerPage}`
+  );
+  const data = helper.emptyOrRows(rows);
+  const meta = {page};
+
+  return {
+    data,
+    meta
+  }
+}
+
+async function create(user){
+  const result = await db.query(
+    `INSERT INTO users 
+    (username, password, email, address, role_id) 
+    VALUES 
+    (${user.username}, ${user.password}, ${user.email}, ${user.address}, ${user.roleId})`
+  );
+
+  let message = 'Error in creating user';
+
+  if (result.affectedRows) {
+    message = 'User created successfully';
+  }
+
+  return {message};
+}
+
+module.exports = {
+  getMultiple,
+  create
+}
