898378499a2eebc69829a7c37d0983f4e2916007
Naoufel Boukri <boukri_n@etna-alternance.net>
2022-12-14 13:49:30 +0100

    wip register

--- Front/src/app/app-routing.module.ts
+++ Front/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserRegisterComponent } from './user/user-register/user-register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'register', component: UserRegisterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
