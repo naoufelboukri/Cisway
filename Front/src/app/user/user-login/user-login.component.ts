b9dc33746bde45c7f814ca300d523732a523a156
said <saadan_s@etna-alternance.net>
2022-12-14 13:17:05 +0100

    Rintialized project minimal

---
+++ Front/src/app/user/user-login/user-login.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-login',
+  template: `
+    <p>
+      user-login works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class UserLoginComponent {
+
+}
