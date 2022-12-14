b3bcca08e2fc1fbf21496710c5afab211f601d34
Naoufel Boukri <boukri_n@etna-alternance.net>
2022-12-14 16:03:33 +0100

    Add bootstrap and navigation bar

--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserModule } from './user/user.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { UserModule } from './user/user.module';
     BrowserModule,
     UserModule,
     AppRoutingModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Front/src/app/user/user-login/user-login.component.ts
+++ Front/src/app/user/user-login/user-login.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
       user-login works!
     </p>
   `,
-  styles: [
+  styles: [ 
   ]
 })
 export class UserLoginComponent {
