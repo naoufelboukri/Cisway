361aa51764910b93f2f9e4b5e2e0ed1346535da9
Naoufel Boukri <boukri_n@etna-alternance.net>
2022-12-31 10:22:22 +0100

    new Front complete environment

--- Front/src/app/app.component.ts
+++ Front/src/app/app.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.ts.html'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  title = 'Front';
 }
--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -1,32 +1,18 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { ProductModule } from './product/product.module';
-import { UserModule } from './user/user.module';
-import { FormLoginComponent } from './form-login/form-login.component';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PageNotFoundComponent,
-    FormLoginComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule,
-    FormsModule,
-    ProductModule,
-    UserModule,
-    AppRoutingModule,
+    AppRoutingModule
   ],
-  providers: [
-    HttpClient
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
